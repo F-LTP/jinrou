3656ece4ac98ee134c9c79c7fc9b35a684b34de2
Nanaya716 <qiyewuyin@gmail.com>
2026-01-18 00:07:45 +0800

    修复白边

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -65,7 +65,7 @@ export const FixedSizeChunkWrapper = styled.div<{
 export const FixedSizeLogRow = styled.div`
   display: grid;
   grid-template-columns: ${fixedSizeGridColumnsPC};
-  margin-bottom: -0.2px;
+  margin-bottom: -0.35px;
   ${phone`
     grid-template-columns: ${fixedSizeGridColumnsPhone};
     grid-auto-flow: row dense;
