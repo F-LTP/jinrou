25c40d4c09d746764eead1535acc617a5ee4c71e
Nanaya716 <qiyewuyin@gmail.com>
2026-01-19 19:36:16 +0800

    真修好了

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -65,6 +65,9 @@ export const FixedSizeChunkWrapper = styled.div<{
 export const FixedSizeLogRow = styled.div`
   display: grid;
   grid-template-columns: ${fixedSizeGridColumnsPC};
+  contain: layout style paint;
+  content-visibility: auto;
+  contain-intrinsic-size: auto 20px;
 
   ${phone`
     grid-template-columns: ${fixedSizeGridColumnsPhone};
