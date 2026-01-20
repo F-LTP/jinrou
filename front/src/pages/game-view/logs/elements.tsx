d9d4dabe0ac38b4e881734917c1b596b8d249ffa
Nanaya716 <qiyewuyin@gmail.com>
2026-01-20 10:35:02 +0800

    表现一致化

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -66,7 +66,6 @@ export const FixedSizeLogRow = styled.div`
   display: grid;
   grid-template-columns: ${fixedSizeGridColumnsPC};
   contain: layout style paint;
-  content-visibility: auto;
   contain-intrinsic-size: auto 20px;
   margin-bottom: -0.35px;
   ${phone`
