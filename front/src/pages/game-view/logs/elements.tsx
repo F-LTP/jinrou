981aa60b9c7e1e28857a336b8ef0d31a2b248609
Nanaya716 <qiyewuyin@gmail.com>
2026-03-25 10:54:36 +0800

    fix:不通过contain:layout去解决卡顿问题，问题似乎出在部分浏览器的Zero-copy rasterizer选项未启用上

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -59,7 +59,6 @@ export const LogWrapper = styled.div<{
 export const FixedSizeChunkWrapper = styled.div<{
   visible: boolean;
 }>`
-  contain: layout style;
   display: ${({ visible }) => (visible ? 'block' : 'none')};
 `;
 
@@ -69,7 +68,6 @@ export const FixedSizeChunkWrapper = styled.div<{
 export const FixedSizeLogRow = styled.div`
   display: grid;
   grid-template-columns: ${fixedSizeGridColumnsPC};
-  contain: layout style;
   margin-bottom: -0.35px;
   ${phone`
     grid-template-columns: ${fixedSizeGridColumnsPhone};
