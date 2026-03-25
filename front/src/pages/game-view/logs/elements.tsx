a8d549995c4af60ddb3f20461bde9b1cf64213a3
Nanaya716 <qiyewuyin@gmail.com>
2026-03-25 13:22:40 +0800

    Revert "fix:不通过contain:layout去解决卡顿问题，问题似乎出在部分浏览器的Zero-copy rasterizer选项未启用上"
    This reverts commit 981aa60b9c7e1e28857a336b8ef0d31a2b248609.

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -59,6 +59,7 @@ export const LogWrapper = styled.div<{
 export const FixedSizeChunkWrapper = styled.div<{
   visible: boolean;
 }>`
+  contain: layout style;
   display: ${({ visible }) => (visible ? 'block' : 'none')};
 `;
 
@@ -68,6 +69,7 @@ export const FixedSizeChunkWrapper = styled.div<{
 export const FixedSizeLogRow = styled.div`
   display: grid;
   grid-template-columns: ${fixedSizeGridColumnsPC};
+  contain: layout style;
   margin-bottom: -0.35px;
   ${phone`
     grid-template-columns: ${fixedSizeGridColumnsPhone};
