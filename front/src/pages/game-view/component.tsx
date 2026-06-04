738d9631e79e074ea57022582d317a52256d48f9
Nanaya716 <qiyewuyin@gmail.com>
2026-06-04 14:26:28 +0800

    滑动问题

--- front/src/pages/game-view/component.tsx
+++ front/src/pages/game-view/component.tsx
@@ -549,7 +549,6 @@ const MainWrapper = styled.div`
   display: flex;
   flex-flow: row nowrap;
   position: relative;
-  overflow-x: hidden;
 `;
 
 /**
@@ -578,14 +577,14 @@ interface IPropsRuleWrapper {
  * Wrapper of rule.
  */
 const RuleWrapper = styled.div<IPropsRuleWrapper>`
+  display: ${({ closed }) => (closed ? 'none' : 'block')};
   position: absolute;
   right: 0;
   top: 0;
-  transition: transform 250ms ease-out;
-  transform: translateX(${({ closed }) => (closed ? '100%' : '0')});
-  pointer-events: ${({ closed }) => (closed ? 'none' : 'auto')};
   width: 20em;
   max-width: 100%;
+  max-height: 100vh;
+  overflow: auto;
   order: 2;
 
   z-index: ${ruleZIndex};
