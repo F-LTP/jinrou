a48e77a7701841b0fb2679c68c5e82e8b082ac1d
Nanaya716 <qiyewuyin@gmail.com>
2026-06-07 17:02:01 +0800

    替换fix样式修改

--- front/src/pages/new-room/elements.tsx
+++ front/src/pages/new-room/elements.tsx
@@ -7,3 +7,14 @@ import { AppStyling } from '../../styles/phone';
 export const Wrapper = styled(AppStyling)`
   padding: 0 20px 20px;
 `;
+
+/**
+ * Wrapper of village rule template controls.
+ */
+export const TemplateControls = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 0.4em;
+  margin-top: 0.4em;
+`;
